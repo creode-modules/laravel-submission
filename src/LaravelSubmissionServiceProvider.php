53f88c51411801ccfa2ba910c1675181e6bdf4c5
Liam Spedding <liam.spedding@creode.co.uk>
2024-01-31 09:44:59 +0000

    Initial commit

---
+++ src/LaravelSubmissionServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Creode\LaravelSubmission;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelSubmissionServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
